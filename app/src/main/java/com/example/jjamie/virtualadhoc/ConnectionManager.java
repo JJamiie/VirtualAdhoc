847ecddb04148b4051b8a7791d31981465a6dbd4
Shimanosk <shimanosk130@hotmail.com>
2016-01-03 23:49:04 +0900

    a

--- app/src/main/java/com/example/jjamie/virtualadhoc/ConnectionManager.java
+++ app/src/main/java/com/example/jjamie/virtualadhoc/ConnectionManager.java
@@ -22,6 +22,7 @@ public class ConnectionManager {
                 ApManager.configApState(context,true);
             }
             else {
+                //connect AP
                 connectAP(context);
             }
 
